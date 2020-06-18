bff4b9f6dde25a534fd0af13b97ab828cd53b044
@rrizaldym <rizky.rizaldy.m@gmail.com>
2020-06-18 15:44:57 +0700

    Upload tugas 4

---
+++ Tugas4-function/function.js
@@ -0,0 +1,29 @@
+//No.1
+console.log('No. 1');
+
+function teriak(){
+    console.log("Halo Sanbers!")
+}
+teriak();
+
+console.log('');
+//No. 2
+console.log('No. 2');
+
+function kalikan(num1, num2){
+    return num1*num2
+}
+
+var hasilKali = kalikan(12, 4);
+console.log(hasilKali);
+
+console.log('');
+//No. 3
+console.log('No. 3');
+
+function introduce(name, age, address, hobby){
+    return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`
+}
+
+var perkenalan = introduce('Agus', 30, 'Jln. Malioboro, Yoyakarta', 'Gaming!');
+console.log(perkenalan);
